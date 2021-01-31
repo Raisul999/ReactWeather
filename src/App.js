f352041e003b94ddd81a15a16976b725421211e0
Raisul999 <raisulislamarnob0@gmail.com>
2021-01-31 21:04:57 +0600

    fixes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import moment from 'moment'
 import './App.css';
+import React, { useState } from 'react';
+const proxy = "https://cors-anywhere.herokuapp.com/"
+const api = {
+  key: "456ac42d13f24b5764e457781128b027",
+  base: "http://api.weatherstack.com/current"
+}
+
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = e => {
+    if (e.key === "Enter") {
+      fetch(`${proxy}${api.base}?access_key=${api.key}&query=${query}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result)
+          setQuery('');
+          console.log(result)
+        })
+
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()]
+    let year = d.getFullYear()
+    let time = d.getHours() + ":" + d.getMinutes() ;
+    // if((d.getHours()==0 || d.getHours() <12)){
+    //   time = time + "AM"
+    // }else{
+    //   time = time + "PM"
+    // }
+    return `${day} ${month} `
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      <main>
+        <div className="search-box" >
+          <input type="text" className="search-bar" placeholder="Enter location"
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.location != "undefined" && typeof weather.current != "undefined") ? (<div>
+          <div className="location-box">
+            <div className="location">{weather.location.name},{weather.location.country}</div>
+            <div className="date">{dateBuilder(new Date())},{moment(weather.location.localtime).format('LT')}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">{weather.current.temperature}°C</div>
+
+            <div className="feel">Feels like:{weather.current.feelslike}°C</div>
+            <div className="weather">{weather.current.weather_descriptions}</div>
+          </div>
+        </div>) : ('')}
+
+      </main>
     </div>
   );
 }
